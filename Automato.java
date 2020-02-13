60b6cd392a915f36bc0d104af5aba047859d4a12
Sadlark1993 <48948852+Sadlark1993@users.noreply.github.com>
2020-02-13 00:20:02 -0400

    Add files via upload

--- Automato.java
+++ Automato.java
@@ -1,5 +1,5 @@
 /*Automato Finito*/
-package automato;
+package mainPackage;
 
 import java.util.Vector;
 import java.io.File;
@@ -9,13 +9,15 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-
 
 public class Automato {
     public static void main(String args[])throws IOException {
-        boolean verbose = true;  //Quando verdadeiro, alguns prints serao ativados para debug e teste
-        String fileName = "C:\\Users\\Aluno.DESKTOP-Q9Q32RU\\Documents\\NetBeansProjects\\Automato\\src\\automato\\automato.txt";
+        String[] instruction = {"1", "2", "1", "2"};
+        boolean verbose = true;  //when true, some data will be printed for debuging process
+        String fileName = "D:\\documentos\\Engenharia_da_computacao\\Compiladores\\NetBeansProjects\\AutomatoFinito\\src\\main\\java\\mainPackage\\automato.txt";
         File arquivo = new File(fileName);
         if(!arquivo.exists()){
             arquivo.createNewFile();
@@ -23,24 +25,23 @@ public class Automato {
         FileReader fr = new FileReader(arquivo);
         BufferedReader br = new BufferedReader(fr);
         
-        Vector<String> texto = new Vector<String>(); //lista encadeada, para adicionar quantos elementos quiser
+        Vector<String> texto = new Vector<String>(); //resizable list, to add values at will ('-')
         
         do{
             String line = br.readLine();
             texto.addElement(line);
         }while(br.ready());
         
-        if(verbose){    //para debug
+        if(verbose){    //for debug
             for(int i = 0; i<texto.size(); i++){
                 System.out.println(texto.get(i));
             }
         }
         
-        /*Abaixo, eu precisava converter uma linha (string) em um vetor com os 
-        caracteres que representam os dados (sem espaços ou vírgulas)*/
-        int j=0;
+        /*Converting a String in a vector with strings that represent the
+        process list*/
         String aux = texto.get(0);
-        aux = aux.replaceAll(" ","");
+        aux = aux.replaceAll(" ", "");
         String[] processos = aux.split(";");
         
         if(verbose){
@@ -51,8 +52,67 @@ public class Automato {
         }
         
         aux = texto.get(1);
+        aux = aux.replaceAll(" ","");
+        String[] states = aux.split(";");
         
+        if(verbose){
+            System.out.println("");
+            for(int i = 0; i<states.length; i++){
+                System.out.println(states[i]);
+            }
+        }
         
+        aux = texto.get(2);
+        String initialValue = aux.trim();
+        
+        if(verbose){
+            System.out.println("\n"+ initialValue);
+        }
+        
+        aux = texto.get(3);
+        String finalValue = aux.trim();
+        
+        if(verbose){
+            System.out.println("\n"+ finalValue);
+            System.out.println("\nTransitionfunction:");
+        }
+        
+        //Here, the transition instruction is built:
+        String[] aux2;
+        
+        Map<String,String> transitionFunction = new HashMap<>();
+        for(int i=4; i<texto.size(); i++){
+            String[] aux3 = new String[2];
+            aux = texto.get(i);
+            aux = aux.replace(" ", "");
+            aux2 = aux.split(";");
+            aux3[0] = aux2[0];
+            aux3[1] = aux2[1];
+            transitionFunction.put(aux3[0]+aux3[1], aux2[2]);
+            if(verbose){
+                System.out.println(aux3[0]+ " "+ aux3[1]+ " "+ transitionFunction.get(aux3[0]+aux3[1]));
+                //System.out.println(transitionFunction.get(aux3));
+            }
+        }
+        String[] aux4 = new String[2];
+        if(verbose){
+            aux4[0] = "B";
+            aux4[1] = "1";
+            System.out.println("teste: "+aux4[0]+ " "+ aux4[1]+ " "+ transitionFunction.get(aux4[0]+aux4[1]));
+            }
+        String currentState = initialValue, process, nextState;
+        System.out.println("\n\n***Iniciando execução***");
+        for(int i = 0; i<instruction.length;i++){
+            aux4[0] = currentState;
+            aux4[1] = instruction[i];
+            nextState = transitionFunction.get(aux4[0]+aux4[1]);
+            System.out.println(aux4[0]+", "+aux4[1]+"-->"+nextState);
+            currentState = nextState;
+            if(currentState.equals(finalValue)){
+                System.out.println("Success!! FinalValue achived!");
+                break;
+            }
+        }
+        System.out.println("end of program (:");  
     }
-    
 }
